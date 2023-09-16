952e12c73363bbb47a0e57b620a1593edc686f98
abrahamayquipa <abrahamayquipa2000@gmail.com>
2023-09-16 15:25:08 -0500

    feat: add labels between nodes

--- grafos.py
+++ grafos.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as matplot
+import random
 
 # Función para mostrar grafo
 def generarGrafo(matriz):
@@ -18,12 +19,17 @@ def generarGrafo(matriz):
         
     # Dibuja aristas y etiquetas
     for i in range(n):
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             if matriz[i, j]:
                 x1, y1 = coordenadas[i]
                 x2, y2 = coordenadas[j]
                 # k significa negro y - indica una línea continua.
                 objeto.plot([x1, x2], [y1, y2], 'k-')
+                # Calcula la posición media entre los dos puntos para la etiqueta
+                posicionMediaEtiquetaX, posicionMediaEtiquetaY = (x1 + x2) / 2, (y1 + y2) / 2
+                valorEntero = random.randint(10, 25)
+                # Imprime el valor de los caminos
+                objeto.text(posicionMediaEtiquetaX, posicionMediaEtiquetaY, str(valorEntero), ha='center', va='center', color='black')
 
     # Nombre de nodos en letras blancas
     for i, (x, y) in enumerate(coordenadas):
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import matrices
-import caminos
 import grafos
 
 def main():
@@ -12,19 +11,13 @@ def main():
     print("a. Generar matriz aleatoria")
     print("b. Generar matriz manualmente")
     opcion = input("Ingresar opcion: ")
-    if opcion.lower() == "a":
-        matriz = matrices.generarMatrizAleatoria(valorIngresado)
-    else:
-        matriz = matrices.generarMatrizManaulmente(valorIngresado)
+
+    if opcion.lower() == "a": matriz = matrices.generarMatrizAleatoria(valorIngresado)
+    else: matriz = matrices.generarMatrizManaulmente(valorIngresado)
+
     print("Matriz generada:")
     print(matriz)
     
-    primerVertice= int(input(f"Seleccione el primer vértice (entre 1 y {valorIngresado}): ")) - 1
-    segundoVertice = int(input(f"Seleccione el segundo vértice (entre 1 y {valorIngresado}): ")) - 1
-
-    camino = caminos.caminoMinimo(matriz, primerVertice, segundoVertice)
-    print(f"El camino mínimo entre {primerVertice + 1} y {segundoVertice + 1} es:\n{' -> '.join(map(str, [x + 1 for x in camino]))}")
-
     grafos.generarGrafo(matriz)
 
 if __name__ == "__main__":
